7c9bc915d86b65b89a2d5754b183cdf240ddd71e
Eder Yafeth Garcia Quiroa <ey16_619@hotmail.com>
2026-02-23 17:18:06 -0600

    fix: Suppress SonarCloud warning for IValueConverter methods
    Added NOSONAR comments to Convert and ConvertBack methods that implement IValueConverter.
    These methods cannot be made static, so the warning from SonarCloud is safely ignored.

--- ReceiptsManagementSystem.Presentation/Converters/LanguageSelectedConverter.cs
+++ ReceiptsManagementSystem.Presentation/Converters/LanguageSelectedConverter.cs
@@ -7,20 +7,11 @@ namespace ReceiptsManagementSystem.Presentation.Converters;
 public class LanguageSelectedConverter : IValueConverter
 {
 
-    /// <summary>
-    /// Converts the source value to the target value.
-    /// Although this method could be static, it must remain an instance method
-    /// to comply with <see cref="IValueConverter"/> for WPF/XAML usage.
-    /// The warning about "make static" can be safely ignored.
-    /// </summary>
+    // NOSONAR: The Convert method cannot be static because it implements IValueConverter
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         => IsSelected(value, parameter);
 
-    /// <summary>
-    /// Conversion back is not implemented. Could be static, but is kept as an instance
-    /// method to satisfy <see cref="IValueConverter"/> requirements in WPF/XAML.
-    /// The warning about "make static" is intentional and safe.
-    /// </summary>
+    // NOSONAR: The Convert method cannot be static because it implements IValueConverter
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         => throw new NotImplementedException();
 
